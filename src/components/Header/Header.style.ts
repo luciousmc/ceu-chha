51ffe11af73df6239a1a05c3d7926782bd666a4e
Marlon Clay <luciousmc@icloud.com>
2022-01-02 03:51:12 -0800

    folder cleanup adds styled-components

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 80px;
+  background-color: purple;
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer } from './Header.style';
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <h1>I am the Header</h1>
+    </HeaderContainer>
+  );
+}
+
+export { Header };
