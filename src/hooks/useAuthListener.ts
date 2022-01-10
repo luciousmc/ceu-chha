949f496c4b878e6e06a176c7d86e1eb0f78b1f5d
Marlon Clay <luciousmc@icloud.com>
2022-01-10 08:43:35 -0800

    adds useAuthListener hook to listen for users' log status

---
+++ src/hooks/useAuthListener.ts
@@ -0,0 +1,25 @@
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { auth } from '../lib/firebase';
+
+export default function useAuthListener() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        localStorage.setItem('authUser', JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return { user };
+}
