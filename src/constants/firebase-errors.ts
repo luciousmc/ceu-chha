2a6e59a025c794df58ab577ddda476a1c3a10755
Marlon Clay <luciousmc@icloud.com>
2022-02-21 17:34:03 -0800

    refactor: change error messages for login form

--- src/constants/firebase-errors.ts
+++ src/constants/firebase-errors.ts
@@ -1,3 +1,4 @@
 export const USER_NOT_FOUND = 'Firebase: Error (auth/user-not-found).';
+export const INVALID_EMAIL = 'Firebase: Error (auth/invalid-email).';
 export const INVALID_PASSWORD = 'Firebase: Error (auth/invalid-password).';
 export const INTERNAL_ERROR = 'Firebase: Error (auth/internal-error).';
--- src/helpers/signInErrorHandler.ts
+++ src/helpers/signInErrorHandler.ts
@@ -4,9 +4,11 @@ const signInErrorHandler = (errorMessage: string) => {
   switch (errorMessage) {
     case FB_ERR.USER_NOT_FOUND:
     case FB_ERR.INVALID_PASSWORD:
-      return 'Invalid username/password combination';
+      return 'Invalid username or password';
     case FB_ERR.INTERNAL_ERROR:
-      return 'Unexpected Server Error. Please try again later';
+      return 'Unexpected server error. Please try again later';
+    case FB_ERR.INVALID_EMAIL:
+      return 'Username is not valid';
     default:
       return errorMessage;
   }
