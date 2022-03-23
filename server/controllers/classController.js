ad37de143a44553b162117c84431644db36c99cf
Marlon Clay <luciousmc@icloud.com>
2022-03-23 01:49:34 -0700

    feat: add method to get all classes

--- server/controllers/classController.js
+++ server/controllers/classController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler');
+const Class = require('../models/classModel');
 
 // @desc    Get all classes
 // @route   GET /api/classes
 // @access  Private
 const getClasses = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getClass function executed' });
+  const res = await (await Class.getClasses()).json();
+
+  res.status(200).json({ message: 'getClass function executed', data: res });
 });
 
 module.exports = {
