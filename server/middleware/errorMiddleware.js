0bb2e7210c0edc396372e7f30bbf83cd1b5fe530
Marlon Clay <luciousmc@icloud.com>
2022-03-23 02:11:48 -0700

    feat: error stack only shows in development

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -2,9 +2,11 @@ const ClientError = require('../util/ClientError');
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof ClientError) {
-    res.status(err.status).json({
-      errorCode: err.code,
+    const status = err.status ? err.status : 500;
+
+    res.status(status).json({
       message: err.message,
+      stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
     });
   } else {
     next(err);
