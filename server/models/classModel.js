6802c8ac38637a2da31283cecba6f9ab30e07552
Marlon Clay <luciousmc@icloud.com>
2022-03-22 20:53:58 -0700

    feat: create schema for db

---
+++ server/models/classModel.js
@@ -0,0 +1,15 @@
+const db = require('../db');
+
+class classModel {
+  constructor(props) {
+    this.topic = props.topic;
+    this.ampm = props.am_pm;
+    this.date = props.date;
+    this.platform = props.platform;
+    this.status = props.status;
+  }
+  static async getClasses() {
+    const res = await db.query('SELECT * FROM classes');
+    return res.rows;
+  }
+}
