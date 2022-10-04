da05bbe4bc436d81d4959b07a68657484feee36f
Marlon Clay <luciousmc@icloud.com>
2022-10-04 11:20:38 -0700

    chore: convert clientError to typescript

---
+++ server/util/ClientError.ts
@@ -0,0 +1,12 @@
+class ClientError extends Error {
+  message: string;
+  status: number;
+
+  constructor(message: string, status: number) {
+    super(message);
+    this.message = message;
+    this.status = status;
+  }
+}
+
+export default ClientError;
