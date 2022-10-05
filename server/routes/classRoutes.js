de1d3799d2951b4c9fddfd85e769990be9670484
Marlon Clay <luciousmc@icloud.com>
2022-10-05 11:21:35 -0700

    chore: remove unused code

--- server/routes/classRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getClasses, createClass } = require('../controllers/classController');
-const router = express.Router();
-
-router.get('/', getClasses);
-router.post('/create', createClass);
-
-module.exports = router;
