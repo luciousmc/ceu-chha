5914174d93feb16bfc52e7cff3a1bf5b1c3d5764
Marlon Clay <luciousmc@icloud.com>
2022-10-12 20:21:46 -0700

    feat(): add app class to server

---
+++ server/app.ts
@@ -0,0 +1,36 @@
+import express, { Application } from 'express';
+import ClassController from './controllers/class copy';
+import errorHandler from './middleware/errorMiddleware';
+
+class App {
+  app: Application;
+  port: number;
+
+  constructor(controllers: ClassController[], port: number) {
+    this.app = express();
+    this.port = port;
+
+    this.loadMiddleware();
+    this.loadControllers(controllers);
+    this.loadErrorHandler();
+  }
+  loadMiddleware() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
+  loadControllers(controllers: ClassController[]) {
+    controllers.forEach((controller) => {
+      this.app.use('/api', controller.router);
+    });
+  }
+  loadErrorHandler() {
+    this.app.use(errorHandler);
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`App is listening on port ${this.port}`);
+    });
+  }
+}
+
+export default App;
