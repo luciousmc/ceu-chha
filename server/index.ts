3843b762155e10fc2730ecd5d69aee12a2bb2bc4
Marlon Clay <luciousmc@icloud.com>
2022-10-16 10:19:57 -0700

    feat(): add student controller

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import App from './app';
 import ClassController from './controllers/class';
+import StudentController from './controllers/student';
 
-const app = new App([new ClassController()], 5001);
+const app = new App([new ClassController(), new StudentController()], 5001);
 app.listen();
