957683208b573ac8a032a827377d9610058c033e
Marlon Clay <luciousmc@icloud.com>
2023-01-10 21:38:04 -0800

    feat: add login function

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,7 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, type Student } from '@prisma/client';
 import { Router } from 'express';
 import IController from '../interfaces/controller';
 import asyncHandler from 'express-async-handler';
+import bcrypt from 'bcrypt';
+import BadCredentialsError from '../util/BadCredentialsError';
+import { excludeFields } from '../util/prisma.helpers';
 
 class AuthController implements IController {
   PATH = '/auth';
@@ -19,7 +22,26 @@ class AuthController implements IController {
   // @desc Logs the user into the app
   // @route POST /auth/login
   // @access public
-  login = asyncHandler(async (req, res, next) => {});
+  login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await this.prisma.student.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user) {
+      const matchPassword = await bcrypt.compare(password, user.password);
+      if (matchPassword) {
+        const userWithoutPw = excludeFields(user, 'password');
+        res.status(200).send(userWithoutPw);
+      } else {
+        next(new BadCredentialsError());
+      }
+    } else {
+      next(new BadCredentialsError());
+    }
+  });
 
   // @desc Logs the user out of the app
   // @route POST /auth/logout
