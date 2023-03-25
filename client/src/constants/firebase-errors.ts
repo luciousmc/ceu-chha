28a486ad50157ff8d9de70891ef2406c6f2260b9
Marlon Clay <luciousmc@icloud.com>
2023-03-25 09:23:55 -0700

    feat: add 'wrong-passsword' firebase error

--- client/src/constants/firebase-errors.ts
+++ client/src/constants/firebase-errors.ts
@@ -1,4 +1,5 @@
 export const USER_NOT_FOUND = 'Firebase: Error (auth/user-not-found).';
 export const INVALID_EMAIL = 'Firebase: Error (auth/invalid-email).';
 export const INVALID_PASSWORD = 'Firebase: Error (auth/invalid-password).';
+export const WRONG_PASSWORD = 'Firebase: Error (auth/wrong-password).';
 export const INTERNAL_ERROR = 'Firebase: Error (auth/internal-error).';
--- client/src/helpers/signInErrorHandler.ts
+++ client/src/helpers/signInErrorHandler.ts
@@ -4,6 +4,7 @@ const signInErrorHandler = (errorMessage: string) => {
   switch (errorMessage) {
     case FB_ERR.USER_NOT_FOUND:
     case FB_ERR.INVALID_PASSWORD:
+    case FB_ERR.WRONG_PASSWORD:
       return 'Invalid username or password';
     case FB_ERR.INTERNAL_ERROR:
       return 'Unexpected server error. Please try again later';
