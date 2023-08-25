806d987558965411db28f2712a5a66e6a2b18e44
Marlon Clay <luciousmc@icloud.com>
2023-08-24 22:51:36 -0700

    chore: remove unused code

--- server/util/NoRegisteredClassesError.ts
+++
@@ -1,9 +0,0 @@
-import ClientError from './ClientError';
-
-class NoRegisteredClassesError extends ClientError {
-  constructor() {
-    super('There are currently no registered classes', 200);
-  }
-}
-
-export default NoRegisteredClassesError;
