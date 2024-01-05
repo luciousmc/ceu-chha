6569b5c64e674cb8c181d029b4f36cfaa1ae6d2b
Marlon Clay <luciousmc@icloud.com>
2024-01-04 17:58:37 -0800

    feat: add StudentCourseInfo type

---
+++ client/src/components/Courses/Courses.types.ts
@@ -0,0 +1,13 @@
+import { CourseInfo } from '../../types/class';
+
+export interface StudentCourseInfo {
+  first_name: string;
+  last_name: string;
+  ceu_license_num: number;
+  ceu_exp_date: string;
+  chha_license_num: number;
+  chha_exp_date: string;
+  registered_classes: number;
+  classes_total: number;
+  classes: CourseInfo[];
+}
