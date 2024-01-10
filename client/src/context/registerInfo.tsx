a7d28b9b51a689b59d87812ad9b8e7f966bc25c5
Marlon Clay <luciousmc@icloud.com>
2024-01-10 14:47:05 -0800

    refactor: change default value for registerinfo context
    Removes all the properties of the students info and replaces it with an empty object

--- client/src/context/registerInfo.tsx
+++ client/src/context/registerInfo.tsx
@@ -1,44 +1,15 @@
-// React
 import { createContext } from 'react';
-
-// Types
 import type { ReactNode } from 'react';
 import type { UseRegisterInfoType } from '../types/registerInfo';
-import type { studentInfoType } from '../types/studentInfo';
-
-// Hooks
+import type { StudentInfoType } from '../types/studentInfo';
 import useRegisterInfo from '../hooks/useRegisterInfo';
 
-// Create Context
 const RegisterInfoContext = createContext<UseRegisterInfoType | null>(null);
 
-// Context Provider
 export const RegisterInfoProvider = ({ children }: { children: ReactNode }) => {
   return (
     <RegisterInfoContext.Provider
-      value={useRegisterInfo({
-        std_fName: '',
-        std_lName: '',
-        std_middleInitial: '',
-        std_ssn: '',
-        std_email: '',
-        std_address: '',
-        std_city: '',
-        std_state: '',
-        std_zip: '',
-        std_birthDate: '',
-        std_phoneNum: '',
-        work_name: '',
-        work_address: '',
-        work_city: '',
-        work_state: '',
-        work_zip: '',
-        work_phoneNum: '',
-        ceu_license_number: '',
-        ceu_license_exp_date: '',
-        chha_license_number: '',
-        chha_license_exp_date: '',
-      })}
+      value={useRegisterInfo({} as StudentInfoType)}
     >
       {children}
     </RegisterInfoContext.Provider>
